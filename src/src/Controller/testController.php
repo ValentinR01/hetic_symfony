3fa3f3d89918cfacf86ad6a5d5c013db382aa40d
Doustalet Chloé <cdoustalet@gmail.com>
2022-10-12 17:28:03 +0200

    Add all templates and test routes

--- src/src/Controller/testController.php
+++ src/src/Controller/testController.php
@@ -7,8 +7,46 @@ use Symfony\Component\HttpFoundation\Response;
 
 class testController extends AbstractController
 {
+    /**
+     * @return Response
+     */
     public function index(): Response
     {
         return $this->render('index.html.twig');
+    }
+
+    /**
+     * @return Response
+     */
+    public function account(): Response{
+        return $this->render('account.html.twig');
+    }
+
+    /**
+     * @return Response
+     */
+    public function createAccount(): Response{
+        return $this->render('createAccount.html.twig');
+    }
+
+    /**
+     * @return Response
+     */
+    public function product(): Response{
+        return $this->render('product.html.twig');
+    }
+
+    /**
+     * @return Response
+     */
+    public function createProduct(): Response{
+        return $this->render('createProduct.html.twig');
+    }
+
+    /**
+     * @return Response
+     */
+    public function allProducts(): Response{
+        return $this->render('allProducts.html.twig');
     }
 }
