3009c39e535419bac65c159881365a490b9d5dec
Doustalet Chloé <cdoustalet@gmail.com>
2022-10-17 19:31:36 +0200

    First form created

---
+++ src/src/Controller/testController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\CreateAccountType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class testController extends AbstractController
+{
+    /**
+     * @return Response
+     */
+    public function index(): Response
+    {
+        return $this->render('index.html.twig');
+    }
+
+    /**
+     * @return Response
+     */
+    public function account(): Response{
+        return $this->render('account.html.twig');
+    }
+
+    /**
+     * @return Response
+     */
+    public function createAccount(): Response{
+        $form = $this->createForm(CreateAccountType::class);
+
+        return $this->render('createAccount.html.twig', [
+
+        ]);
+    }
+
+    /**
+     * @return Response
+     */
+    public function product(): Response{
+        return $this->render('product.html.twig');
+    }
+
+    /**
+     * @return Response
+     */
+    public function createProduct(): Response{
+        return $this->render('createProduct.html.twig');
+    }
+
+    /**
+     * @return Response
+     */
+    public function allProducts(): Response{
+        return $this->render('allProducts.html.twig');
+    }
+}
---
+++ src/src/Form/CreateAccountType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CreateAccountType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('pseudo')
+            ->add('email')
+            ->add('photo')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+        ]);
+    }
+}
