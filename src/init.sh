7430b95cd03b2022fc2fa3ee4da406c0c61ec8a5
ValentinR01 <valentin.rejaunier@hetic.net>
2022-10-27 12:12:59 +0200

    fixture-launch-automation

--- src/init.sh
+++ src/init.sh
@@ -4,5 +4,7 @@ echo 'Creating Database - If not exists'
 php bin/console doctrine:database:create --if-not-exists
 echo 'Create Migration'
 php bin/console doctrine:migration:migrate
+echo 'Create Fixtures'
+yes | symfony console doctrine:fixtures:load
 echo 'Starting Server'
 nginx && php-fpm
