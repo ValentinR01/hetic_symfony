1deaff4d1811915e1f23bc5565932d578a100fcb
lestox <78364535+lestox@users.noreply.github.com>
2022-10-30 18:34:44 +0100

    test

--- src/public/js/form.js
+++ src/public/js/form.js
@@ -1,6 +1,13 @@
 var questionButton = document.getElementById('askQuestionBtn')
 var questionForm = document.getElementById('commentForm')
 
+var questionAnswerButton = document.getElementById('askQuestionAnswerBtn')
+var questionAnswerForm = document.getElementById('commentAnswerForm')
+
 function displayForm(){
     questionForm.style.display = 'block'
+}
+
+function displayAnswerForm(){
+    questionAnswerForm.style.display = 'block'
 }
